8cb8eb91251c26c0a5009f0fbbede290d80e4044
jpmarceaux <johnpaulmarceaux@gmail.com>
2024-10-21 12:55:06 -0700

    additions and updates

--- utility.py
+++ utility.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def angular_difference(phi1, phi2):
+def angular_difference(phi1, phi2, domain=2*np.pi):
     # returns the distance between two angles measured in radians 
     diff = phi2 - phi1
-    return np.mod(diff + np.pi, 2*np.pi) - np.pi
+    return np.mod(diff + domain/2, domain) - domain/2
